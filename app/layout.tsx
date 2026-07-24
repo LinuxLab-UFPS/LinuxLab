94a6469cb8a97f2c6bde02c58602138a03e2cee8
Mauricio <mauriciodidonato794@gmail.com>
2026-07-24 10:36:46 -0400

    feat(theme): crossfade colors on dark/light toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -45,7 +45,6 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="light"
           enableSystem={false}
-          disableTransitionOnChange
         >
           <AuthProvider>
             {children}
